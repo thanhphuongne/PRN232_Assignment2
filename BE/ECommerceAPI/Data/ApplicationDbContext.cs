5c7a1f1979caef27b5d19266e030960485ecb9ee
thanhphuongne <lephuong14052004@gmail.com>
2025-10-15 16:28:35 +0700

    rename

--- BE/ECommerceAPI/Data/ApplicationDbContext.cs
+++ BE/ECommerceAPI/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ECommerceAPI.Models;
@@ -18,6 +19,42 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        // Customize Identity table names
+        builder.Entity<ApplicationUser>(entity =>
+        {
+            entity.ToTable("Users");
+        });
+
+        builder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable("Roles");
+        });
+
+        builder.Entity<IdentityUserRole<string>>(entity =>
+        {
+            entity.ToTable("UserRoles");
+        });
+
+        builder.Entity<IdentityUserClaim<string>>(entity =>
+        {
+            entity.ToTable("UserClaims");
+        });
+
+        builder.Entity<IdentityUserLogin<string>>(entity =>
+        {
+            entity.ToTable("UserLogins");
+        });
+
+        builder.Entity<IdentityRoleClaim<string>>(entity =>
+        {
+            entity.ToTable("RoleClaims");
+        });
+
+        builder.Entity<IdentityUserToken<string>>(entity =>
+        {
+            entity.ToTable("UserTokens");
+        });
 
         // Configure relationships
         builder.Entity<Order>()
