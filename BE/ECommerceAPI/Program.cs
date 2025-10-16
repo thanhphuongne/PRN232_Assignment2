1d30e8842bf0badbeebbdb57b66404e9a7e5b01c
thanhphuongne <lephuong14052004@gmail.com>
2025-10-16 10:00:05 +0700

    Fix CORS to allow credentials for frontend authentication

--- BE/ECommerceAPI/Program.cs
+++ BE/ECommerceAPI/Program.cs
@@ -58,6 +58,7 @@ builder.Services.AddCors(options =>
         policy.AllowAnyOrigin()
               .AllowAnyMethod()
               .AllowAnyHeader()
+              .AllowCredentials()
               .WithExposedHeaders("Access-Control-Allow-Origin");
     });
 });
